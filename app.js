827b807f373719c0cc5402d0629d4123f138b093
Jane <cjmendez26@gmail.com>
2020-01-25 10:09:39 -0800

    app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+var mysql = require("mysql");
+var inquirer = require('inquirer');
+var password;
+
+inquirer.prompt([
+    {
+        type: "password",
+        message: "Please enter your root password",
+        name: "password"
+    }
+]).then(function (answers) {
+    password = answers.password;
+    console.log(typeof password);
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        // Your username
+        user: "root",
+        // Your password
+        password: password,
+        database: "greatBay"
+    });
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId);
+        postOrBidSelector();
+        connection.end();
+    });
+});
+
+function postOrBidSelector() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "userOption",
+            message: "Please select from the follwoing:",
+            choices: ["Post Item", "Place Bid"]
+        }
+    ]).then(function (answers) {
+        let userOption = answers.userOption;
+        if (userOption == "Post Item") {
+            console.log("Post item f(x)")
+        }
+        console.log("Place Bid f(x)");
+
+    });
+};
+
+
+function bid (){
+    console.log("this is f(x) bid");
+
+};
+
+function post () {
+    console.log("this is f(x) post item")
+    
+};
